c1f1215cfc316a642ee31e7ea95c95b045b65410
jfsicilia <jfsicilia@gmail.com>
2015-09-11 16:05:07 +0100

    Only try to create symlink if there is no one.

--- install/link.sh
+++ install/link.sh
@@ -6,6 +6,8 @@ echo "-- creating symlinks"
 linkables=$( find -H "$DOTFILES" -maxdepth 3 -name '*.symlink' )
 for file in $linkables ; do
     target="$HOME/.$( basename $file ".symlink" )"
-    echo "-- creating symlink for $file"
-    ln -s $file $target
+    if ! test -h $target; then
+        echo "-- creating symlink for $file in home"
+        ln -s $file $target
+    fi
 done
