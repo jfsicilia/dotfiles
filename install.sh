7b68eb54a976d8ff37795effa425ee10e22ac6c8
jfsicilia <jfsicilia@gmail.com>
2015-09-13 04:58:22 +0100

    Added colored echo.

--- install.sh
+++ install.sh
@@ -1,9 +1,31 @@
 #!/bin/bash
 
-echo -e "\n#######################"
-echo "# Installing dotfiles #"
-echo "#######################"
-echo "-- Init install.sh"
+# Set the colours you can use
+black='\033[0;30m'
+white='\033[0;37m'
+red='\033[0;31m'
+green='\033[0;32m'
+yellow='\033[0;33m'
+blue='\033[0;34m'
+magenta='\033[0;35m'
+cyan='\033[0;36m'
+
+# Resets the style
+reset=`tput sgr0`
+
+# Color-echo. Improved. [Thanks @joaocunha]
+# arg $1 = message
+# arg $2 = Color
+cecho() {
+  echo -e "${2}${1}${reset}"
+  return
+}
+
+echo ""
+cecho "#######################" $blue
+cecho "# Installing dotfiles #" $blue
+cecho "#######################" $blue
+cecho "Init install.sh" $green
 
 # Ask for the administrator password upfront.
 sudo -v
@@ -12,56 +34,65 @@ sudo -v
 while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
 
-echo "-- Initializing submodule(s)"
+cecho "Initializing submodule(s)" $green
 git submodule update --init --recursive
 
-echo -e "\n###################################################"
-echo "# Creating simbolic links in HOME to config files #"
-echo "###################################################"
+echo ""
+cecho "###################################################" $blue
+cecho "# Creating simbolic links in HOME to config files #" $blue
+cecho "###################################################" $blue
 source install/link.sh
 
 if [ "$(uname)" == "Darwin" ]; then
-    echo -e "\n#####################################################"
-    echo "# Installing XCode Command Line Tools for compiling #"
-    echo "#####################################################"
+    echo ""
+    cecho "#####################################################" $blue
+    cecho "# Installing XCode Command Line Tools for compiling #" $blue
+    cecho "#####################################################" $blue
     xcode-select --install
-    echo "-- Press any key to continue after installing XCode CLT..."
+    cecho "Press any key to continue after installing XCode CLT..." $green
     read -n 1 -s
 
-    echo -e "\n##########################"
-    echo "# Brewing all the things #"
-    echo "##########################"
+    echo ""
+    cecho "##########################" $blue
+    cecho "# Brewing all the things #" $blue
+    cecho "##########################" $blue
     source install/brew.sh
 
-    echo -e "\n#########################"
-    echo "# Updating OSX settings #"
-    echo "#########################"
+    echo ""
+    cecho "#########################" $blue
+    cecho "# Updating OSX settings #" $blue
+    cecho "#########################" $blue
     source install/osx.sh
 
-    echo -e "\n##############################"
-    echo "# Mackup restore apps config #"
-    echo "##############################"
+    echo ""
+    cecho "##############################" $blue
+    cecho "# Mackup restore apps config #" $blue
+    cecho "##############################" $blue
     mackup restore
 fi
 
-echo -e "\n###########################"
-echo "# Install powerline fonts #"
-echo "###########################"
+echo ""
+cecho "###########################" $blue
+cecho "# Install powerline fonts #" $blue
+cecho "###########################" $blue
 source install/powerline.sh
 
-echo -e "\n#######################"
-echo "# Customize oh-my-zsh #"
-echo "#######################"
+echo ""
+cecho "#######################" $blue
+cecho "# Customize oh-my-zsh #" $blue
+cecho "#######################" $blue
 source install/oh-my-zsh.sh
 
-echo -e "\n############################"
-echo "# Set zsh as default shell #"
-echo "############################"
+echo ""
+cecho "############################" $blue
+cecho "# Set zsh as default shell #" $blue
+cecho "############################" $blue
 source install/zsh.sh
 
-echo -e "\n#########################"
-echo "# Updating Vim settings #"
-echo "#########################"
+echo ""
+cecho "#########################" $blue
+cecho "# Updating Vim settings #" $blue
+cecho "#########################" $blue
 source install/vi.sh
 
-echo -e "\n-- Done install.sh."
+echo "-- Done install.sh."
