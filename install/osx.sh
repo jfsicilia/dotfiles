05c7ef5fc20c434201e49a45baaf1ba38f622d9b
jfsicilia <jfsicilia@gmail.com>
2015-09-13 04:13:47 +0100

    Added cecho functionality

--- install/osx.sh
+++ install/osx.sh
@@ -1,4 +1,26 @@
 #!/usr/bin/env bash
+
+# Set the colours you can use
+black='\033[0;30m'
+white='\033[0;37m'
+red='\033[0;31m'
+green='\033[0;32m'
+yellow='\033[0;33m'
+blue='\033[0;34m'
+magenta='\033[0;35m'
+cyan='\033[0;36m'
+
+# Resets the style
+reset=`tput sgr0`
+
+# Color-echo. Improved. [Thanks @joaocunha]
+# arg $1 = message
+# arg $2 = Color
+cecho() {
+  echo "${2}${1}${reset}"
+  return
+}
+
 echo "-- Init osx.sh"
 
 DOTFILES="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd )"
@@ -877,7 +899,8 @@ for app in "Activity Monitor" "Address Book" "Calendar" "Contacts" "cfprefsd" \
 	killall "${app}" > /dev/null 2>&1
 done
 
-echo "-- Note that some of these changes require a logout/restart to take effect."
+cecho "-- Note that some of these changes require a logout/restart to take
+effect." $red
 echo "-- Done osx.sh"
 
 
